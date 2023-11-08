969bb21e61484f02a6f05a30c5dc43d84c1abfff
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-11-08 22:13:13 +0800

    :art: Format 3rd homework

--- src/TCPClient.c
+++ src/TCPClient.c
@@ -1,45 +1,42 @@
+#if 0
+bin="$(basename "$0")" && bin="${bin%%.*}" && gcc "$0" -o"$bin" && exec ./"$bin" "$@"
+#endif
 #include <arpa/inet.h>
+#include <err.h>
+#include <errno.h>
+#include <libgen.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <time.h>
 #include <unistd.h>
 
 #define MAXLINE 1024
-#define TRUE 1
-int main(int argc, char** argv) {
-	int sockfd, n, m;
-	char line[MAXLINE + 1];
-	struct sockaddr_in6 servaddr;
-	time_t t0 = time(NULL);
-	printf("time #: %ld\n", t0);
-	fputs(ctime(&t0), stdout);
-	if (argc != 2)
-		// 检测 main()函数是不是只有一个参数输入
-		perror("usage: a.out <IPaddress>");
-	// 调用 socket()函数，得到对应套接口的文件描述符 sockfd；如果调用失败，perror 会显示错误信息，然后非正常退出
-	if ((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
-		perror("socket error");
-	// 套接口信息初始化
-	// 结构体其余清零
-	bzero(&servaddr, sizeof(servaddr));
-	// ADDRESS FAMILY 地址族
-	servaddr.sin6_family = AF_INET6;
-	// 把输入整数转换成“网络字节顺序”
-	servaddr.sin6_port = htons(20000);
-	// 将输入参数转换成二进制格式，并存入 servaddr.sin6_addr
-	if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0)
-		perror("inet_pton error");
-	// 连接正在监听的服务器
-	if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
-		perror("connect error");
-	// 发送数据
-	while (fgets(line, MAXLINE, stdin) != NULL) {
-		send(sockfd, line, strlen(line), 0);
-	}
-	// 关闭套接口
-	close(sockfd);
-	exit(0);
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("usage: %s <IPaddress>\n", basename(argv[0]));
+    return EXIT_SUCCESS;
+  }
+
+  int sd = socket(AF_INET6, SOCK_STREAM, 0);
+  if (sd == -1)
+    err(errno, NULL);
+
+  struct sockaddr_in6 sa = {
+      .sin6_family = AF_INET6,
+      .sin6_port = htons(20000),
+  };
+  if (inet_pton(AF_INET6, argv[1], &sa.sin6_addr) == -1)
+    err(errno, NULL);
+
+  if (connect(sd, (struct sockaddr *)&sa, sizeof(sa)) == -1)
+    errx(errno, "run TCPServer firstly!");
+
+  char line[MAXLINE + 1];
+  while (fgets(line, MAXLINE, stdin))
+    send(sd, line, strlen(line), 0);
+
+  return close(sd);
 }
--- src/TCPServer.c
+++ src/TCPServer.c
@@ -1,58 +1,44 @@
+#if 0
+bin="$(basename "$0")" && bin="${bin%%.*}" && gcc "$0" -o"$bin" && exec ./"$bin" "$@"
+#endif
 #include <arpa/inet.h>
+#include <err.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <time.h>
 #include <unistd.h>
-#define MAXLINE 1024 // 最大通信信息行数
-#define TRUE 1
-int main(int argc, char** argv) {
-	int sockfd, fd, n, m;
-	char line[MAXLINE + 1];
-	// 定义记录己方和客户端的套接口地址信息的结构体
-	struct sockaddr_in6 servaddr, cliaddr;
-	time_t t0 = time(NULL);
-	printf("time #: %ld\n", t0);
-	fputs(ctime(&t0), stdout);
-	// 调用 socket()函数，得到对应套接口的文件描述符 sockfd；如果调用失败，perror 会显示错误信息，然后非正常退出
-	if ((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
-		perror("socket error");
-	// 套接口信息初始化
-	// 结构体其余清零
-	bzero(&servaddr, sizeof(servaddr));
-	// ADDRESS FAMILY 地址族
-	servaddr.sin6_family = AF_INET6;
-	// 把输入整数转换成“网络字节顺序”
-	servaddr.sin6_port = htons(20000);
-	// 自动填上所运行的机器的 IP 地址
-	servaddr.sin6_addr = in6addr_any;
-	// 使用 bind()将上面得到的套接口绑定到本地某个端口，如果失败，显示错误原因；成功就接着运行
-	if (bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1)
-		perror("bind error");
-	// listen()监听，等待接入请求；其中 5 是进入队列中允许的连接数目
-	if (listen(sockfd, 5) == -1)
-		perror("listen error");
 
-	while (TRUE) {
-		printf("> Waiting clients ...\r\n");
-		socklen_t clilen = sizeof(struct sockaddr);
-		// fd 描述接收连接(accept())
-		fd = accept(sockfd, (struct sockaddr*)&cliaddr, &clilen);
-		if (fd == -1) {
-			perror("accept error");
-		}
-		printf("> Accepted.\r\n");
-		while ((n = read(fd, line, MAXLINE)) > 0) {
-			line[n] = 0;
-			if (fputs(line, stdout) == EOF)
-				// 服务器端打印读入的信息
-				perror("fputs error");
-		}
-		close(fd);
-	}
-	if (n < 0)
-		perror("read error");
-	exit(0);
+#define MAXLINE 1024
+
+int main(int argc, char **argv) {
+  int sd = socket(AF_INET6, SOCK_STREAM, 0);
+  if (sd == -1)
+    err(errno, NULL);
+
+  struct sockaddr_in6 sa = {
+      .sin6_family = AF_INET6,
+      .sin6_port = htons(20000),
+      .sin6_addr = in6addr_any,
+  };
+
+  if (bind(sd, (struct sockaddr *)&sa, sizeof(sa)) == -1)
+    err(errno, NULL);
+
+  if (listen(sd, 5) == -1)
+    err(errno, NULL);
+
+  while (1) {
+    int fd = accept(sd, NULL, NULL);
+    if (fd == -1)
+      err(errno, NULL);
+
+    char line[MAXLINE + 1] = "";
+    while (read(fd, line, MAXLINE) > 0)
+      puts(line);
+
+    close(fd);
+  }
 }
